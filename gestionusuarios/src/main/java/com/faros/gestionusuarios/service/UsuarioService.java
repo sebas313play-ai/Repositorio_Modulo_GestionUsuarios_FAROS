4ceb2eef366d4b0a0e4028fac1eb180292faf036
Julian Camilo Mosquera <juliancamilomosquera6@gmail.com>
2026-06-30 20:31:23 -0500

    feat(user): define UsuarioService interface for CRUD operations

--- gestionusuarios/src/main/java/com/faros/gestionusuarios/service/UsuarioService.java
+++ gestionusuarios/src/main/java/com/faros/gestionusuarios/service/UsuarioService.java
@@ -1,4 +1,20 @@
 package com.faros.gestionusuarios.service;
 
-public class UsuarioService {
+import com.faros.gestionusuarios.dto.*;
+
+import java.util.List;
+
+public interface UsuarioService {
+
+    UsuarioResponse guardar(UsuarioRequest request);
+
+    List<UsuarioResponse> listar();
+
+    UsuarioResponse buscar(Long id);
+
+    UsuarioResponse actualizar(Long id,
+            UsuarioRequest request);
+
+    void eliminar(Long id);
+
 }
