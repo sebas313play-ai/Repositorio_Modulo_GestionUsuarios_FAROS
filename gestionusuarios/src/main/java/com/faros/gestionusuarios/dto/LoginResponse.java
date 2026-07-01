50218524eda0a17ce1a78dcd6229fad4f9bd6b94
Julian Camilo Mosquera <juliancamilomosquera6@gmail.com>
2026-06-30 20:29:35 -0500

    feat(auth): add LoginResponse DTO to return JWT token

---
+++ gestionusuarios/src/main/java/com/faros/gestionusuarios/dto/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.faros.gestionusuarios.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+
+@AllArgsConstructor
+
+public class LoginResponse {
+
+    private String token;
+
+}
